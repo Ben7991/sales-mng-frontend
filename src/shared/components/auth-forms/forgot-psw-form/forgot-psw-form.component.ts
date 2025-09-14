215a1b18d24636ab043c9b4bd2f1b4cdd489f6fa
jaakumasi <jerome.akumasi@amalitech.com>
2025-09-14 14:03:45 +0000

    feat: implement password reset link request functionality

--- src/shared/components/auth-forms/forgot-psw-form/forgot-psw-form.component.ts
+++ src/shared/components/auth-forms/forgot-psw-form/forgot-psw-form.component.ts
@@ -1,8 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {InputComponent} from "@shared/components/input/input.component";
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {ButtonComponent} from '@shared/components/button/button.component';
-import {RouterLink} from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Router, RouterLink } from '@angular/router';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { InputComponent } from "@shared/components/input/input.component";
+import { TOAST_MESSAGES } from '@shared/constants/general.constants';
+import { NAVIGATION_ROUTES } from '@shared/constants/navigation.constant';
+import { SnackbarService } from '@shared/services/snackbar/snackbar.service';
+import { AuthService } from 'pages/auth/services/auth.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-forgot-psw-form',
@@ -17,15 +23,38 @@ import {RouterLink} from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ForgotPswFormComponent {
-  public forgotPswForm: FormGroup;
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+  private readonly snackbarService = inject(SnackbarService);
+  private readonly fb = inject(FormBuilder);
 
-  constructor(private fb: FormBuilder) {
-    this.forgotPswForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-    });
-  }
+  protected readonly isSubmittingForm = signal(false);
 
-  public onRequestPassReset() {
-      console.log('submitted:', this.forgotPswForm.value);
+  protected readonly forgotPswForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
+
+  protected readonly loginRoute = NAVIGATION_ROUTES.AUTH.LOGIN;
+
+  public onRequestPassReset(): void {
+    if (!this.forgotPswForm.valid) {
+      return;
+    }
+
+    this.isSubmittingForm.set(true);
+
+    this.authService.requestPasswordResetLink(this.forgotPswForm.value)
+      .pipe(
+        finalize(() => this.isSubmittingForm.set(false))
+      )
+      .subscribe({
+        next: (response) => {
+          this.snackbarService.showInfo(response.message);
+        },
+        error: (err: HttpErrorResponse) => {
+          const msg = err.error.message ?? TOAST_MESSAGES.HTTP_ERROR;
+          this.snackbarService.showError(msg);
+        }
+      })
   }
 }
