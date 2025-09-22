e2423583955f923564fe67087e7f426e202047b7
BlessAhadjie <blaise0ahadjie@gmail.com>
2025-09-22 13:40:58 +0000

    feat: make page title update automatic according to url

--- src/shared/components/header/header.component.ts
+++ src/shared/components/header/header.component.ts
@@ -4,8 +4,9 @@ import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {TitleCasePipe, UpperCasePipe} from '@angular/common';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
 import {NAVIGATION_ROUTES} from '@shared/constants/navigation.constant';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -32,6 +33,8 @@ export class HeaderComponent implements OnInit {
   @Input() toggleSidenavCallback: () => void = () => {};
   private readonly destroyRef = inject(DestroyRef);
   private cdr = inject(ChangeDetectorRef);
+  private readonly location = inject(Location);
+
 
   ngOnInit() {
     this.authService.getUserDetails()
@@ -45,6 +48,54 @@ export class HeaderComponent implements OnInit {
           this.cdr.markForCheck();
         },
       });
+
+    const initialPath = window.location.pathname || this.router.url || this.location.path();
+    this.setPageTitleFromUrl(initialPath);
+
+    this.router.events
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          const url = (event as NavigationEnd).urlAfterRedirects || (event as NavigationEnd).url;
+          this.setPageTitleFromUrl(url);
+        }
+      });
+  }
+
+  private setPageTitleFromUrl(rawUrl: string | null | undefined) {
+    if (!rawUrl) {
+      this.pageTitle = '';
+      this.cdr.markForCheck();
+      return;
+    }
+    const path = rawUrl.split('?')[0].split('#')[0];
+
+    const segments = path.split('/').filter(Boolean);
+
+    const mainIndex = segments.indexOf('main');
+    let candidate: string;
+    if (mainIndex !== -1 && segments.length > mainIndex + 1) {
+      candidate = segments[mainIndex + 1];
+    } else if (segments.length) {
+      candidate = segments[segments.length - 1];
+    } else {
+      candidate = '';
+    }
+
+    this.pageTitle = this.formatSegmentToTitle(candidate);
+    this.cdr.markForCheck();
+  }
+
+  private formatSegmentToTitle(segment: string): string {
+    if (!segment) return '';
+    const spaced = segment
+      .replace(/[-_]+/g, ' ')
+      .replace(/([a-z0-9])([A-Z])/g, '$1 $2');
+
+    return spaced
+      .split(' ')
+      .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+      .join(' ');
   }
 
   public navigateToSettings(): void {
