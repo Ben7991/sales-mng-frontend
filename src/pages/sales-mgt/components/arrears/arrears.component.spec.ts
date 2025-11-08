fee1c117d78bdddd4a0c7f78318b33efc5ccd09c
BlessAhadjie <blaise0ahadjie@gmail.com>
2025-11-08 14:49:32 +0000

    feat: implement arrears component

---
+++ src/pages/sales-mgt/components/arrears/arrears.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArrearsComponent } from './arrears.component';
+
+describe('ArrearsComponent', () => {
+  let component: ArrearsComponent;
+  let fixture: ComponentFixture<ArrearsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ArrearsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ArrearsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
